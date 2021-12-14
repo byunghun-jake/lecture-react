43257382d0239c09665c957a39ccfe5bad8f99f6
김병훈 <figma@kakao.com>
2021-12-14 11:07:02 +0900

    조건부 렌더링: element 변수

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,8 +1,45 @@
-const element = (
-  <header>
-    <h2 className="container">검색</h2>
-  </header>
-  // TODO
-);
+class App extends React.Component {
+  constructor() {
+    super()
 
-ReactDOM.render(element, document.querySelector("#app"));
+    this.state = {
+      searchKeyword: "",
+    }
+  }
+
+  handleChangeInput(event) {
+    this.setState({
+      searchKeyword: event.target.value,
+    })
+  }
+
+  render() {
+    let resetButton = null
+
+    if (this.state.searchKeyword.length > 0) {
+      resetButton = <button type="reset" className="btn-reset"></button>
+    }
+
+    return (
+      <>
+        <header>
+          <h2 className="container">검색</h2>
+        </header>
+        <div className="container">
+          <form id="search-form-view">
+            <input
+              type="text"
+              placeholder="검색어를 입력하세요"
+              autoFocus
+              value={this.state.searchKeyword}
+              onChange={(event) => this.handleChangeInput(event)}
+            />
+            {resetButton}
+          </form>
+        </div>
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"))
